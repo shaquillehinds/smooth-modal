406ac978972950278371bc628dcf9f920e00734c
Shaquille <shaqdulove@hotmail.com>
2025-03-26 23:29:01 -0400

    lower zIndex

--- src/BottomModal/bottomModal.styles.ts
+++ src/BottomModal/bottomModal.styles.ts
@@ -2,7 +2,10 @@ import { StyleSheet } from 'react-native';
 import { modalBottomOffset, modalHeight } from './bottomModal.constants';
 import { isIOS, relativeX, relativeY } from '../utils/Layout.const';
 
-const zIndex = Number.MAX_SAFE_INTEGER - 100;
+// 999999999 - This should be enought for most cases, 9 nines
+// 700000000000000 - This works but I would still use a lower number
+// 9007199244740991 - MAX_SAFE_INTERGER doesn't work sometimes
+const zIndex = 999999999;
 
 export const bottomModalStyle = StyleSheet.create({
   topLevelContainer: { zIndex },
