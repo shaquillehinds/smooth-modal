7266bdbd2a0db03784911d42b97c39dc278910dc
Shaquille <shaqdulove@hotmail.com>
2025-03-30 00:22:02 -0400

    added package react-native-svg

--- src/SmoothModal/svgs/Notification.tsx
+++ src/SmoothModal/svgs/Notification.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Svg, { Path } from 'react-native-svg';
 import { normalize } from '../utils/Layout.const';
 
@@ -16,7 +15,8 @@ export function Notification({
       width={normalize(size || 24)}
       height={normalize(size || 24)}
       viewBox="0 0 24 24"
-      fill="none">
+      fill="none"
+    >
       <Path
         d="M19 8C20.6569 8 22 6.65685 22 5C22 3.34315 20.6569 2 19 2C17.3431 2 16 3.34315 16 5C16 6.65685 17.3431 8 19 8Z"
         fill={color2 || '#292D32'}
