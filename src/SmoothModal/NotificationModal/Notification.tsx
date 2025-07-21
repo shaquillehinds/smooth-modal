cb1b7a450cc8e37f0c23d4438efbcde6fbdd91f0
Shaquille <shaqdulove@hotmail.com>
2025-07-21 10:27:00 -0400

    notification modal custom component unpressable bug fix

--- src/SmoothModal/NotificationModal/Notification.tsx
+++ src/SmoothModal/NotificationModal/Notification.tsx
@@ -47,6 +47,78 @@ try {
 export function Notification(props: NotificationProps) {
   const controller = NotificationController(props);
   const content = props.notification.content;
+
+  const Component = (
+    <SwipeGesture onActivation={controller.onSwipeUp} direction="UP">
+      <Animated.View
+        style={[
+          { height: notificationHeight },
+          notificationStyles.notification,
+          props.notificationStyle,
+          props.notification.style,
+          controller.notifAnimatedStyle,
+        ]}
+      >
+        {content.type === 'component' ? (
+          content.payload
+        ) : (
+          <View
+            style={[
+              notificationStyles.notificationContent,
+              { height: notificationHeight },
+              content.payload.contentContainerStyle,
+            ]}
+          >
+            {content.payload.image ? (
+              <View>
+                <ImageComponent
+                  //@ts-ignore
+                  contentFit="contain"
+                  resizeMode={type === 'react-native' ? undefined : 'contain'}
+                  style={[
+                    notificationStyles.notificationImage,
+                    content.payload.imageStyle,
+                  ]}
+                  source={
+                    typeof content.payload.image === 'string'
+                      ? { uri: content.payload.image }
+                      : content.payload.image
+                  }
+                />
+              </View>
+            ) : content.payload.Icon ? (
+              <View>{content.payload.Icon}</View>
+            ) : undefined}
+            <View>
+              {content.payload.title ? (
+                <Text
+                  style={[
+                    notificationStyles.notificationTitle,
+                    content.payload.titleStyle,
+                  ]}
+                  numberOfLines={content.payload.message ? 1 : 2}
+                >
+                  {content.payload.title}
+                </Text>
+              ) : undefined}
+
+              {content.payload.message ? (
+                <Text
+                  numberOfLines={content.payload.title ? 2 : 3}
+                  style={[
+                    notificationStyles.notificationMessage,
+                    content.payload.messageStyle,
+                  ]}
+                >
+                  {content.payload.message}
+                </Text>
+              ) : undefined}
+            </View>
+          </View>
+        )}
+      </Animated.View>
+    </SwipeGesture>
+  );
   return (
     <ComponentMounter
       key={props.notification.id}
@@ -56,88 +128,21 @@ export function Notification(props: NotificationProps) {
       unMountDelayInMilliSeconds={controller.notificationDurationMilliS}
       onComponentClose={controller.onComponentClose}
       component={
-        <TouchableOpacity
-          activeOpacity={props.notification.activeOpacity || 0.8}
-          onPress={() => {
-            props.notification.onPress &&
-              props.notification.onPress(
+        props.notification.onPress ? (
+          <TouchableOpacity
+            activeOpacity={props.notification.activeOpacity || 0.8}
+            onPress={() => {
+              props.notification.onPress?.(
                 props.notification.content,
                 controller.onSwipeUp
               );
-          }}
-        >
-          <SwipeGesture onActivation={controller.onSwipeUp} direction="UP">
-            <Animated.View
-              style={[
-                { height: notificationHeight },
-                notificationStyles.notification,
-                props.notificationStyle,
-                props.notification.style,
-                controller.notifAnimatedStyle,
-              ]}
-            >
-              {content.type === 'component' ? (
-                content.payload
-              ) : (
-                <View
-                  style={[
-                    notificationStyles.notificationContent,
-                    { height: notificationHeight },
-                    content.payload.contentContainerStyle,
-                  ]}
-                >
-                  {content.payload.image ? (
-                    <View>
-                      <ImageComponent
-                        //@ts-ignore
-                        contentFit="contain"
-                        resizeMode={
-                          type === 'react-native' ? undefined : 'contain'
-                        }
-                        style={[
-                          notificationStyles.notificationImage,
-                          content.payload.imageStyle,
-                        ]}
-                        source={
-                          typeof content.payload.image === 'string'
-                            ? { uri: content.payload.image }
-                            : content.payload.image
-                        }
-                      />
-                    </View>
-                  ) : content.payload.Icon ? (
-                    <View>{content.payload.Icon}</View>
-                  ) : undefined}
-                  <View>
-                    {content.payload.title ? (
-                      <Text
-                        style={[
-                          notificationStyles.notificationTitle,
-                          content.payload.titleStyle,
-                        ]}
-                        numberOfLines={content.payload.message ? 1 : 2}
-                      >
-                        {content.payload.title}
-                      </Text>
-                    ) : undefined}
-
-                    {content.payload.message ? (
-                      <Text
-                        numberOfLines={content.payload.title ? 2 : 3}
-                        style={[
-                          notificationStyles.notificationMessage,
-                          content.payload.messageStyle,
-                        ]}
-                      >
-                        {content.payload.message}
-                      </Text>
-                    ) : undefined}
-                  </View>
-                </View>
-              )}
-            </Animated.View>
-          </SwipeGesture>
-        </TouchableOpacity>
+            }}
+          >
+            {Component}
+          </TouchableOpacity>
+        ) : (
+          Component
+        )
       }
     />
   );
